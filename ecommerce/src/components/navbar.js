999b21be37c282a79a3fdb1d7e9ae76715a3bd9c
unknown <ericaddojunior100@gmail.com>
2024-01-01 04:26:07 +0000

    2024

--- ecommerce/src/components/navbar.js
+++ ecommerce/src/components/navbar.js
@@ -1,15 +1,43 @@
 import React from "react";
-import '../styles.css'
+import '../styles.css';
+import logo from '../images/logo.svg';
+import bag from '../images/bag.svg';
+import heart from '../images/heart.svg';
+import search from '../images/search.svg';
+
 
 
 export default function Navbar(){
     return(
-        <section>
-            <nav>
-                <h1 className="navfirsttext">TThis is Just the beginning</h1>
-                <h2>This the the start of something big RENDAR</h2>
+        <>
+            <nav className="navbar">
+                <div>
+                    <img className="logo" src={logo} alt="this is the logo"/>
+                </div>
+                <div>
+                <ul className="navtexts">
+                    <li>New arrivals</li>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>Kids</li>
+                    <li>Collection</li>
+                </ul>
+                </div>
+                <div>
+                    <span className="dividingline"></span>
+                    <img className="search" src={search} alt="search" />
+                    <span className="dividingline"></span>
+                    <img className="heart" src={heart} alt="heart"/>
+                    <span className="dividingline"></span>
+                    <img className="bag" src={bag} alt="bag"/>
+                    <span className="dividingline"></span>
+                </div>
             </nav>
-        </section>
+            <section className="secondsection">
+                <p className="firsttext">Members : Free Shipping on Orders $50+</p>
+                <p className="secondtext">Join now</p>
+            </section>
+            </>
 
     );
 }
