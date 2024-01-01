2a9b25df29982b49b2e9c7b9a716e462c876f16e
unknown <ericaddojunior100@gmail.com>
2024-01-01 02:46:33 +0000

    2024

---
+++ ecommerce/src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles.css'
+
+
+export default function Navbar(){
+    return(
+        <section>
+            <nav>
+                <h1 className="navfirsttext">TThis is Just the beginning</h1>
+                <h2>This the the start of something big RENDAR</h2>
+            </nav>
+        </section>
+
+    );
+}
